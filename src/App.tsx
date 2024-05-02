3d507632a9b7650b6ca4e6318ddc7d4a709d2077
ki8vi <oneofbln@gmail.com>
2024-05-02 10:40:11 +0600

    chore: set huasky and pre-commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { greet } from './test';
 
-greet();
 function App() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
 
     return (
         <>
