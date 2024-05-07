b5b6b2797733e21e2d853f561f2f42429cf103ce
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-05-08 01:21:49 +0500

    feat: some change in layout

--- src/components/app-registartion/app-registration.tsx
+++ src/components/app-registartion/app-registration.tsx
@@ -2,19 +2,22 @@ import './app-registration.css';
 
 const RegistrationLayout = () => {
     return (
-        <form className="app-registration">
-            <div className="registration-client-info">
-                <input className="registration-email field" placeholder="Почта" required />
-                <input className="registration-name field" placeholder="Имя" required />
-                <input className="registration-surname field" placeholder="Фамилия" required />
-                <input className="registration-birthday field" type="date" placeholder="Дата рождения" required />
-            </div>
+        <form className="app-registration-form">
+            <div className="registration-form-container">
+                <div className="registration-client-info">
+                    <input className="registration-email field" type="email" placeholder="Почта" required />
+                    <input className="registration-email field" type="password" placeholder="Пароль" required />
+                    <input className="registration-name field" placeholder="Имя" required />
+                    <input className="registration-surname field" placeholder="Фамилия" required />
+                    <input className="registration-birthday field" type="date" placeholder="Дата рождения" required />
+                </div>
 
-            <div className="registration-address-fields">
-                <input className="registration-street field" placeholder="Улица" required />
-                <input className="registration-city field" placeholder="Город" required />
-                <input className="registration-postalcode field" placeholder="Почтовый Индекс" required />
-                <input className="registration-country field" placeholder="Страна" required />
+                <div className="registration-address-fields">
+                    <input className="registration-street field" placeholder="Улица" required />
+                    <input className="registration-city field" placeholder="Город" required />
+                    <input className="registration-postalcode field" placeholder="Почтовый Индекс" required />
+                    <input className="registration-country field" placeholder="Страна" required />
+                </div>
             </div>
 
             <button className="registration-btn-submit" type="submit">
