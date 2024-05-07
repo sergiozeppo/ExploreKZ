ad692d38e54e96925d9d79979e9fe8ebd84d0b69
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-05-07 19:29:08 +0500

    feat: add layout for registration, export component

---
+++ src/components/app-registartion/app-registration.tsx
@@ -0,0 +1,27 @@
+import './app-registration.css';
+
+const RegistrationLayout = () => {
+    return (
+        <form className="app-registration">
+            <div className="registration-client-info">
+                <input className="registration-email field" placeholder="Почта" required />
+                <input className="registration-name field" placeholder="Имя" required />
+                <input className="registration-surname field" placeholder="Фамилия" required />
+                <input className="registration-birthday field" type="date" placeholder="Дата рождения" required />
+            </div>
+
+            <div className="registration-address-fields">
+                <input className="registration-street field" placeholder="Улица" required />
+                <input className="registration-city field" placeholder="Город" required />
+                <input className="registration-postalcode field" placeholder="Почтовый Индекс" required />
+                <input className="registration-country field" placeholder="Страна" required />
+            </div>
+
+            <button className="registration-btn-submit" type="submit">
+                Отправить
+            </button>
+        </form>
+    );
+};
+
+export default RegistrationLayout;
