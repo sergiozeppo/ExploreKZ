3e815ad22d3144e8f639e9cbcc0b1d9fef483b2b
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-05-10 12:04:05 +0500

    feat: add fieldsAboutDelivery

--- src/components/registration/registration.tsx
+++ src/components/registration/registration.tsx
@@ -16,17 +16,23 @@ function Registration() {
         width: '100%',
     } as CSSProperties;
 
-    const fields = [
+    const fieldsAboutUser = [
         { name: 'email', label: 'Почта' },
         { name: 'password', label: 'Имя' },
         { name: 'surname', label: 'Фамилия' },
         { name: 'date', label: 'Дата Рождения', type: 'date' },
     ];
 
+    const fieldsAboutDelivery = [
+        { name: 'street', label: 'Улица' },
+        { name: 'city', label: 'Город' },
+        { name: 'postalCode', label: 'Post Код' },
+    ];
+
     return (
         <form style={{ ...flexStyles, margin: '0 auto', maxWidth: '600px', gap: '5px' }}>
             <div style={{ ...flexStyles, gap: '5px' }}>
-                {fields.map((field) => (
+                {fieldsAboutUser.map((field) => (
                     <div key={field.name}>
                         <label>
                             {field.label}:
@@ -40,14 +46,31 @@ function Registration() {
                         </label>
                     </div>
                 ))}
-                <select
-                    {...register('city', {
-                        required: 'Поле обязательно к заполнению',
-                    })}
-                    style={inputItems}
-                >
-                    <option value="Kazahstan">Kazahstan</option>
-                </select>
+                <fieldset>
+                    <legend>Адрес доставки</legend>
+                    {fieldsAboutDelivery.map((field) => (
+                        <div key={field.name}>
+                            <label>
+                                {field.label}
+                                <input
+                                    {...register(field.name, {
+                                        required: 'Поле обязательно к заполнению',
+                                    })}
+                                    style={inputItems}
+                                />
+                            </label>
+                        </div>
+                    ))}
+
+                    <select
+                        {...register('city', {
+                            required: 'Поле обязательно к заполнению',
+                        })}
+                        style={inputItems}
+                    >
+                        <option value="Kazahstan">Kazahstan</option>
+                    </select>
+                </fieldset>
             </div>
 
             <button className="registration-btn-submit" type="submit">
