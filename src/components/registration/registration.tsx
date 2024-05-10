0553583a50ea330ae13e10e771ce07a7f39bc5cc
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-05-10 23:07:17 +0500

    feat: add some classes and styles for fields

--- src/components/registration/registration.tsx
+++ src/components/registration/registration.tsx
@@ -13,66 +13,70 @@ function Registration() {
 
     return (
         <form className="registration-wrapper flex-style">
-            <div className="registration-wrapper flex-style">
-                {fieldsAboutUser.map((field) => (
-                    <div key={field.name}>
+            {fieldsAboutUser.map((field) => (
+                <div key={field.name} className="registration-container">
+                    <input
+                        {...register(field.name, {
+                            required: 'Поле обязательно к заполнению',
+                        })}
+                        type={field.type}
+                        placeholder={field.placeholder}
+                        className="registration-about-user"
+                    />
+                </div>
+            ))}
+            <fieldset className="registration-wrapper-delivery">
+                <legend>Адрес доставки</legend>
+                <div className="registration-conatiner-pair">
+                    <select
+                        {...register('country', {
+                            required: 'Поле обязательно к заполнению',
+                        })}
+                        className="registration-delivery"
+                    >
+                        <option value="Выберите страну*" disabled selected>
+                            Выберите страну*
+                        </option>
+                        <option value="Kazahstan">Kazahstan</option>
+                    </select>
+                    <input
+                        {...register('region', {
+                            required: 'Поле обязательно к заполнению',
+                        })}
+                        placeholder="Регион"
+                        className="registration-delivery"
+                    />
+                </div>
+                <div className="registration-conatiner-pair">
+                    <div key="city">
                         <input
-                            {...register(field.name, {
+                            {...register('city', {
                                 required: 'Поле обязательно к заполнению',
                             })}
-                            type={field.type}
-                            placeholder={field.placeholder}
+                            placeholder="Город"
+                            className="registration-delivery"
                         />
                     </div>
-                ))}
-                <fieldset>
-                    <legend>Адрес доставки</legend>
-                    <div className="registration-conatiner-pair">
-                        <select
-                            {...register('country', {
-                                required: 'Поле обязательно к заполнению',
-                            })}
-                        >
-                            <option value="Выберите страну*" disabled selected>
-                                Выберите страну*
-                            </option>
-                            <option value="Kazahstan">Kazahstan</option>
-                        </select>
+                    <div key="postalCode">
                         <input
-                            {...register('region', {
+                            {...register('postalCode', {
                                 required: 'Поле обязательно к заполнению',
                             })}
-                            placeholder="Регион"
+                            placeholder="Post Код"
+                            className="registration-delivery"
                         />
                     </div>
-                    <div className="registration-conatiner-pair">
-                        <div key="city">
-                            <input
-                                {...register('city', {
-                                    required: 'Поле обязательно к заполнению',
-                                })}
-                                placeholder="Город"
-                            />
-                        </div>
-                        <div key="postalCode">
-                            <input
-                                {...register('postalCode', {
-                                    required: 'Поле обязательно к заполнению',
-                                })}
-                                placeholder="Post Код"
-                            />
-                        </div>
-                    </div>
-                    <div key="street">
-                        <input
-                            {...register('address', {
-                                required: 'Поле обязательно к заполнению',
-                            })}
-                            placeholder="Адрес"
-                        />
-                    </div>
-                </fieldset>
-            </div>
+                </div>
+                <div key="street">
+                    <input
+                        {...register('address', {
+                            required: 'Поле обязательно к заполнению',
+                        })}
+                        placeholder="Адрес"
+                        className="registration-delivery"
+                    />
+                </div>
+            </fieldset>
 
             <button className="registration-btn-submit" type="submit">
                 Отправить
