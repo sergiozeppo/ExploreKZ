e387af32b40e05b882abe2df264386caea87ff19
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-05-16 11:32:08 +0500

    feat: add test for token component

---
+++ src/apiSdk/token.test.ts
@@ -0,0 +1,23 @@
+import { token } from './token';
+
+describe('token component from apiSdk', () => {
+    it('can create a new component with empty token', () => {
+        const testToken1 = token;
+        expect(testToken1.get()).toEqual({
+            token: '',
+            expirationTime: 0,
+            refreshToken: '',
+        });
+    });
+
+    it('can set and get token', () => {
+        const testToken2 = token;
+        const newToken = {
+            token: 'new-token',
+            expirationTime: 123,
+            refreshToken: 'new-refresh-token',
+        };
+        testToken2.set(newToken);
+        expect(testToken2.get()).toEqual(newToken);
+    });
+});
