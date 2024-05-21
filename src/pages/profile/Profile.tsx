0be8341eea2618dbfe7d31b09f962fd2ae76cb66
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-05-21 19:52:02 +0500

    feat: add layout for profile user info and styles

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,7 +1,17 @@
+import './profile.css';
+import { Img } from '../../components';
+
 export default function Profile() {
     return (
-        <>
-            <p className="tempr">Profile</p>
-        </>
+        <div className="container-profile">
+            <div className="profile-content">
+                <div className="profile-user-info">
+                    <Img src="images/avatar.jpg" alt="Simple Avatar Image" className="user-info-avatar"></Img>
+                    <span className="user-info-name">First Name Last Name</span>
+                    <span className="user-info-name">Date of birth</span>
+                </div>
+                <div className="profile-user-addresses"></div>
+            </div>
+        </div>
     );
 }
