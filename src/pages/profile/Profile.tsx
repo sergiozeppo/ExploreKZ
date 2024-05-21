3e85035d09423176c0fb28acf2131a44557d43e8
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-05-21 20:07:07 +0500

    feat: add first version layout and styles for profile user addresses

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -5,12 +5,43 @@ export default function Profile() {
     return (
         <div className="container-profile">
             <div className="profile-content">
-                <div className="profile-user-info">
+                <div className="profile-user-info profile-user-container">
                     <Img src="images/avatar.jpg" alt="Simple Avatar Image" className="user-info-avatar"></Img>
                     <span className="user-info-name">First Name Last Name</span>
                     <span className="user-info-name">Date of birth</span>
                 </div>
-                <div className="profile-user-addresses"></div>
+                <div className="profile-user-addresses profile-user-container">
+                    <fieldset className="user-addresses-container">
+                        <legend>Delivery address</legend>
+                        <div className="user-addresses-row">
+                            <span>Country:</span>
+                        </div>
+                        <div className="user-addresses-row">
+                            <span>City:</span>
+                        </div>
+                        <div className="user-addresses-row">
+                            <span>Street:</span>
+                        </div>
+                        <div className="user-addresses-row">
+                            <span>Postal Code:</span>
+                        </div>
+                    </fieldset>
+                    <fieldset className="user-addresses-container">
+                        <legend>Billing address</legend>
+                        <div className="user-addresses-row">
+                            <span>Country:</span>
+                        </div>
+                        <div className="user-addresses-row">
+                            <span>City:</span>
+                        </div>
+                        <div className="user-addresses-row">
+                            <span>Street:</span>
+                        </div>
+                        <div className="user-addresses-row">
+                            <span>Postal Code:</span>
+                        </div>
+                    </fieldset>
+                </div>
             </div>
         </div>
     );
