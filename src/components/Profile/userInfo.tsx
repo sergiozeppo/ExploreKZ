5932c77e71287d0be23a3788ef37abc9e025e032
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-05-24 01:47:35 +0500

    feat: add displayName

--- src/components/Profile/userInfo.tsx
+++ src/components/Profile/userInfo.tsx
@@ -3,12 +3,25 @@ import { IUserInfo } from './typesProfile';
 const UserInfo: React.FC<IUserInfo> = (props) => {
     const date = Object.keys(props) as Array<keyof IUserInfo>;
 
+    const displayName = (prop: keyof IUserInfo): string => {
+        switch (prop) {
+            case 'email':
+                return 'Email:';
+            case 'firstName':
+                return 'First Name:';
+            case 'lastName':
+                return 'Last Name:';
+            case 'dateOfBirth':
+                return 'Date of Birth:';
+        }
+    };
+
     console.log(date);
     return (
         <div className="profile-user-cols-container">
             {date.map((prop) => (
                 <div className="profile-user-col">
-                    <span>{prop}</span>
+                    <span>{displayName(prop)}</span>
                     <span className="user-info-name">{props[prop]}</span>
                 </div>
             ))}
