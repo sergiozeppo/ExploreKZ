7bc56fcff97e321fbfbbb8e731803b43a2df263b
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-05-26 12:43:47 +0500

    fix: fix incoming changes

--- src/components/ProductCard/Card.tsx
+++ src/components/ProductCard/Card.tsx
@@ -15,11 +15,32 @@ export const Card = (product: CARD_PROPS) => {
     return (
         <Link to={'/products/' + product.id}>
             <div className="card">
+                {product.discount ? (
+                    <img className="discount-icon" src="./images/icons8-discount-48.png" alt="discount" />
+                ) : (
+                    ''
+                )}
                 <div className="img-wrapper">
                     <img className="card-img" src={product.images} alt={product.name} />
                 </div>
                 <span className="product-title">{product.name}</span>
                 <p className="product-description">{product.description}</p>
+                <div className="price-wrapper">
+                    <span className="product-price">
+                        Price:{' '}
+                        <span className={product.discount ? 'product-price-original' : 'product-price-discount'}>
+                            {product.price}
+                        </span>{' '}
+                        USD
+                    </span>
+                    {product.discount ? (
+                        <span className="discount-wrapper">
+                            New price: <span className="product-price-discount">{product.discount}</span> USD
+                        </span>
+                    ) : (
+                        ''
+                    )}
+                </div>
             </div>
         </Link>
     );
