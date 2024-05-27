822f3e73b28500eb9ebecc84cf45c053cd388951
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-05-28 02:04:34 +0500

    feat: add nodemailer

--- src/pages/changePassword/index.tsx
+++ src/pages/changePassword/index.tsx
@@ -5,7 +5,7 @@ export default function ChangePassword() {
         <div className="conatiner-change-password">
             <div className="window-change-password">
                 <form className="form-change-password">
-                    <fieldset>
+                    <fieldset className="fieldset--change-password">
                         <legend>Change Password</legend>
                         <input className="input-change-password" placeholder="Input Code" type="text" disabled />
                         <button className="profile-btn" type="submit">
