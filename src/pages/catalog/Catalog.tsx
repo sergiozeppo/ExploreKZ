f5d67b3663e258c0852c73f6b5fb615a849c1963
ki8vi <oneofbln@gmail.com>
2024-05-27 23:54:03 +0600

    feat: add filter for products

--- src/pages/catalog/Catalog.tsx
+++ src/pages/catalog/Catalog.tsx
@@ -6,56 +6,110 @@ import { Card } from '../../components/ProductCard/Card';
 import './catalog.css';
 import Loader from '../../components/Loader/loader';
 import { CustomToast } from '../../components/Toast';
+import { TiArrowSortedDown, TiArrowSortedUp } from 'react-icons/ti';
+
 export default function Catalog() {
     const [products, setProducts] = useState<ProductProjection[]>([]);
     const [loading, setLoading] = useState(true);
+    const [currItem, setCurrItem] = useState('All');
+
+    const getProducts = (filter: string) => {
+        setLoading(true);
+        const userToken = localStorage.getItem('userToken');
+        const client = localStorage.getItem('isLogin') && userToken ? tokenClient() : anonUser();
+
+        let queryArgs = {};
+        if (filter === 'Tours') {
+            queryArgs = { filter: 'categories.id:subtree("2eb7506a-8e2b-41c0-934a-27beb1e6d056")' };
+        } else if (filter === 'Adventures') {
+            queryArgs = { filter: 'categories.id:subtree("f7b72444-abd3-4bfa-82b6-473cb991c907")' };
+        }
+
+        client
+            .productProjections()
+            .search()
+            .get({ queryArgs })
+            .execute()
+            .then((res) => {
+                let response: ProductProjection[] = res.body.results;
+
+                if (filter === 'Discounted') {
+                    response = response.filter((product) =>
+                        product.masterVariant?.prices?.some((price) => price.discounted),
+                    );
+                }
+
+                setProducts(response);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+                setLoading(false);
+                CustomToast('error', 'Server problem!');
+            });
+    };
 
     useEffect(() => {
-        if (localStorage.getItem('isLogin')) {
-            const userToken = localStorage.getItem('userToken');
-            if (userToken) {
-                tokenClient()
-                    .productProjections()
-                    .get({
-                        headers: {
-                            Authorization: `Bearer ${JSON.parse(userToken).token}`,
-                        },
-                    })
-                    .execute()
-                    .then((res) => {
-                        const response: ProductProjection[] = res.body.results;
-                        setProducts(response);
-                        setLoading(false);
-                    })
-                    .catch((err) => {
-                        console.error(err);
-                        setLoading(false);
-                        CustomToast('error', 'Server problem!');
-                    });
-            }
-        } else {
-            console.log('anonim flow');
-            anonUser()
-                .productProjections()
-                .get()
-                .execute()
-                .then((res) => {
-                    console.log(res);
-                    const response: ProductProjection[] = res.body.results;
-                    setProducts(response);
-                    setLoading(false);
-                })
-                .catch((err) => {
-                    console.error(err);
-                    setLoading(false);
-                    CustomToast('error', 'Server problem!');
-                });
+        getProducts(currItem);
+    }, [currItem]);
+
+    const [open, setOpen] = useState(false);
+    const [priceRangeOpen, setPriceRangeOpen] = useState(false);
+    const handleClick = () => {
+        setOpen(!open);
+    };
+    const hadleFilterItem = (e: React.MouseEvent<HTMLUListElement>) => {
+        e.stopPropagation();
+        const target = e.target as HTMLLIElement;
+        if (target.getAttribute('id')) {
+            setCurrItem(target.getAttribute('id') || 'All');
+            setOpen(false);
         }
-    }, []);
+    };
+    const handlePrice = (e: React.MouseEvent<HTMLDivElement>) => {
+        e.stopPropagation();
+        setPriceRangeOpen(!priceRangeOpen);
+    };
 
     return (
         <>
-            <p className="tempr">Catalog</p>
+            <div className="filter-area">
+                <div className="filter-title" onClick={handleClick}>
+                    {currItem}
+                    {open ? <TiArrowSortedUp /> : <TiArrowSortedDown />}
+                    {open && (
+                        <ul className="filter-menu" onClick={(e) => hadleFilterItem(e)}>
+                            <li className="filter-item" id="All">
+                                All
+                            </li>
+                            <li className="filter-item" id="Tours">
+                                Tours
+                            </li>
+                            <li className="filter-item" id="Adventures">
+                                Adventures
+                            </li>
+                            <li className="filter-item" id="Discounted">
+                                Discounted
+                            </li>
+                        </ul>
+                    )}
+                </div>
+                <div className="price-filter" onClick={(e) => handlePrice(e)}>
+                    Price
+                    {priceRangeOpen ? <TiArrowSortedUp /> : <TiArrowSortedDown />}
+                    {priceRangeOpen && (
+                        <div className="price-menu">
+                            <div className="range-area">
+                                <ul className="price-inputs">
+                                    <li className="filter-item" id="All">
+                                        0
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
             {loading ? (
                 <Loader />
             ) : (
