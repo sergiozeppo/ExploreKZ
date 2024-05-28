f3299fd4a95e4daad03ae66b6b6851a4e36ec69e
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-05-28 12:54:47 +0500

    feat: add handleSumbitChangePassword

--- src/pages/changePassword/index.tsx
+++ src/pages/changePassword/index.tsx
@@ -1,14 +1,47 @@
 import './index.css';
+import { useState } from 'react';
+import { baseClient } from '../../apiSdk/BaseClient';
 
 export default function ChangePassword() {
+    const [currentPassword, setCurrentPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+
+    const handleSumbitChangePassword = () => {
+        const api = baseClient();
+        try {
+            api.customers()
+                .password()
+                .post({
+                    body: {
+                        id: '1',
+                        version: 1,
+                        currentPassword: currentPassword,
+                        newPassword: newPassword,
+                    },
+                });
+        } catch (error) {
+            return error as Error;
+        }
+    };
+
     return (
         <div className="conatiner-change-password">
             <div className="window-change-password">
-                <form className="form-change-password">
+                <form className="form-change-password" onSubmit={handleSumbitChangePassword}>
                     <fieldset className="fieldset--change-password">
                         <legend>Change Password</legend>
-                        <input className="input-change-password" placeholder="Current Password" type="password" />
-                        <input className="input-change-password" placeholder="New Password" type="password" />
+                        <input
+                            className="input-change-password"
+                            placeholder="Current Password"
+                            type="password"
+                            onChange={(e) => setCurrentPassword(e.target.value)}
+                        />
+                        <input
+                            className="input-change-password"
+                            placeholder="New Password"
+                            type="password"
+                            onChange={(e) => setNewPassword(e.target.value)}
+                        />
                         <button className="btn change-password-btn" type="submit">
                             Change Password
                         </button>
