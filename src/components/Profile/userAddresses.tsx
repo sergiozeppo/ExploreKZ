ffda18d6ae2d467735c6c4238ade610cd592af6e
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-05-29 23:33:19 +0500

    feat: add validation

--- src/components/Profile/userAddresses.tsx
+++ src/components/Profile/userAddresses.tsx
@@ -4,6 +4,7 @@ import { FieldErrors, UseFormRegister } from 'react-hook-form';
 import { UserParams } from '../../apiSdk/RegistrationUser';
 import { FaEdit } from 'react-icons/fa';
 import { MdDelete } from 'react-icons/md';
+import { validate } from '../Validation';
 
 interface UserAddressesAttributes {
     errors: FieldErrors;
@@ -21,25 +22,48 @@ function UserAddresses({ errors, register }: UserAddressesAttributes) {
                 <div className="user-addresses-col">
                     <span>Country:</span>
                 </div>
-                <input name="country" className="user-addresses-input" />
+                <input
+                    {...register('country', {
+                        required: 'This field is required',
+                    })}
+                    className="user-addresses-input"
+                />
             </div>
             <div className="user-addresses-row">
                 <div className="user-addresses-col">
                     <span>Street:</span>
                 </div>
-                <input name="street" className="user-addresses-input" />
+                <input
+                    {...register('streetName', {
+                        required: 'This field is required',
+                        validate: validate['street'],
+                    })}
+                    className="user-addresses-input"
+                />
             </div>
             <div className="user-addresses-row">
                 <div className="user-addresses-col">
                     <span>City:</span>
                 </div>
-                <input name="city" className="user-addresses-input" />
+                <input
+                    {...register('city', {
+                        required: 'This field is required',
+                        validate: validate['city'],
+                    })}
+                    className="user-addresses-input"
+                />
             </div>
             <div className="user-addresses-row">
                 <div className="user-addresses-col">
                     <span>Postal Code:</span>
                 </div>
-                <input name="postalCode" className="user-addresses-input" />
+                <input
+                    {...register('postalCode', {
+                        required: 'This field is required',
+                        validate: validate['postalCode'],
+                    })}
+                    className="user-addresses-input"
+                />
             </div>
             <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '5px' }}>
                 <FormControlLabel
