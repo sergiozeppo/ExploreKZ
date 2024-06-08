40d8193097e6924f774fdeebd606a5487f8a221d
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-06-08 22:34:36 +0500

    feat: add first block of Top Destination component

--- src/components/TopDestinations/index.tsx
+++ src/components/TopDestinations/index.tsx
@@ -1,5 +1,6 @@
 import { Img } from '../Img';
 import { Text } from '../Text';
+import { Link } from 'react-router-dom';
 
 export default function TopDestinations() {
     return (
@@ -10,34 +11,28 @@ export default function TopDestinations() {
             <div className="top-dest-items-wrapper">
                 <div className="top-dest-items-col">
                     <div className="top-dest-items-left">
-                        <div className="relative h-[273px] md:h-auto">
-                            <Img
-                                src="images/img_image_3.png"
-                                alt="image three"
-                                className="h-[273px] w-full rounded-[16px] object-cover"
-                            />
-                            <div className="absolute bottom-[0.00px] left-0 right-0 m-auto flex w-full flex-col items-start gap-[7px] rounded-[14px] bg-gradient pb-7 pl-[62px] pr-14 pt-[15px] md:px-5 sm:pb-5">
-                                <Text as="p" className="!font-poppins">
-                                    Moraine Lake
-                                </Text>
-                                <Text as="p" className="!font-poppins">
-                                    Alberta, Canada
-                                </Text>
+                        <Link to="/catalog/adventures/multi-day/d74c4ce0-cd13-470a-b716-99474483df00">
+                            <div className="top-dest-top-left">
+                                <Img src="/images/kolsay.jpg" alt="Kolsay lakes" className="top-dest-horizontal-img" />
+                                <div className="top-dest-item-div">
+                                    <Text as="p" className="top-dest-item-name">
+                                        Kolsai Lake
+                                    </Text>
+                                    <Text as="p" className="top-dest-item-place">
+                                        Almaty region
+                                    </Text>
+                                </div>
                             </div>
-                        </div>
+                        </Link>
                         <div className="flex gap-[34px] md:flex-col">
                             <div className="relative h-[273px] w-full md:h-auto">
-                                <Img
-                                    src="images/img_image_6.png"
-                                    alt="image six"
-                                    className="h-[273px] w-full rounded-[16px] object-cover"
-                                />
+                                <Img src="images/img_image_6.png" alt="image six" className="top-dest-horizontal-img" />
                                 <div className="absolute bottom-[0.00px] left-0 right-0 m-auto w-full rounded-[14px] bg-gradient pb-[15px] pl-[29px] pr-[42px] md:pr-5 sm:px-5">
                                     <div className="flex flex-col items-start gap-[7px]">
-                                        <Text as="p" className="w-full !font-poppins leading-[33px]">
+                                        <Text as="p" className="w-full top-dest-item-place leading-[33px]">
                                             Hokokuji Bamboo Forest
                                         </Text>
-                                        <Text as="p" className="!font-poppins">
+                                        <Text as="p" className="top-dest-item-place">
                                             Kamakura, Japan
                                         </Text>
                                     </div>
@@ -47,13 +42,13 @@ export default function TopDestinations() {
                                 <Img
                                     src="images/img_image_5.png"
                                     alt="image five"
-                                    className="h-[273px] w-full rounded-[16px] object-cover"
+                                    className="top-dest-horizontal-img"
                                 />
                                 <div className="absolute bottom-[0.00px] left-0 right-0 m-auto flex w-full flex-col items-start gap-1 rounded-[14px] bg-gradient px-[29px] pb-1.5 pt-4 sm:px-5">
-                                    <Text as="p" className="!font-poppins">
+                                    <Text as="p" className="top-dest-item-place">
                                         Kirkjufell
                                     </Text>
-                                    <Text as="p" className="w-[61%] !font-poppins leading-6 md:w-full">
+                                    <Text as="p" className="w-[61%] top-dest-item-place leading-6 md:w-full">
                                         Grundarfjordur&#39;s, Iceland
                                     </Text>
                                 </div>
