75d1a441e9d08dfe9eefbbe0bd99fa10dcab274a
ki8vi <oneofbln@gmail.com>
2024-06-10 12:53:22 +0600

    feat: add 'add to cart' button for cards

--- src/components/ProductCard/Card.tsx
+++ src/components/ProductCard/Card.tsx
@@ -3,6 +3,7 @@ import './card.css';
 import { Link } from 'react-router-dom';
 import { productIdData } from './productIdData';
 import { useEffect, useState } from 'react';
+import { MdOutlineAddShoppingCart } from 'react-icons/md';
 
 type CARD_PROPS = {
     id: string;
@@ -25,6 +26,14 @@ export const Card = (product: CARD_PROPS) => {
         };
         handlePath();
     });
+    const handleAddToCart = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.stopPropagation();
+        e.preventDefault();
+        const currentProduct = e.target as HTMLElement;
+        if (currentProduct.getAttribute('id')) {
+            console.log(currentProduct.getAttribute('id'));
+        }
+    };
     return (
         // <Link to={`${`/products`}/${product.id}`}>
         <Link to={`${path}`}>
@@ -39,21 +48,26 @@ export const Card = (product: CARD_PROPS) => {
                 </div>
                 <span className="product-title">{product.name}</span>
                 <p className="product-description">{product.description}</p>
-                <div className="price-wrapper">
-                    <span className="product-price">
-                        Price:{' '}
-                        <span className={product.discount ? 'product-price-original' : 'product-price-discount'}>
-                            {product.price}
-                        </span>{' '}
-                        USD
-                    </span>
-                    {product.discount ? (
-                        <span className="discount-wrapper">
-                            New price: <span className="product-price-discount">{product.discount}</span> USD
+                <div className="card-bottom-side">
+                    <div className="price-wrapper">
+                        <span className="product-price">
+                            Price:{' '}
+                            <span className={product.discount ? 'product-price-original' : 'product-price-discount'}>
+                                {product.price}
+                            </span>{' '}
+                            USD
                         </span>
-                    ) : (
-                        ''
-                    )}
+                        {product.discount ? (
+                            <span className="discount-wrapper">
+                                New price: <span className="product-price-discount">{product.discount}</span> USD
+                            </span>
+                        ) : (
+                            ''
+                        )}
+                    </div>
+                    <button className="button add-to-cart-btn" onClick={handleAddToCart} id={product.id}>
+                        Add <MdOutlineAddShoppingCart />
+                    </button>
                 </div>
             </div>
         </Link>
