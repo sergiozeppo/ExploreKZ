b6dc713124f80fa3f83f20c1a14e02fde7b1a03a
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-06-14 01:42:13 +0500

    feat: add About us page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -27,37 +27,44 @@ export default function About() {
                         bio="I graduated with a degree in Computer Science. I have been studying frontend development
                             since November 2023"
                         contributions={[
-                            'Added development scripts and set up repository',
+                            'Added development scripts, set up repository and Trello environment',
                             'Set up Jest and perform test suites for entire project',
-                            'Developed Main, About Us, Product details pages',
+                            'Developed Main, About Us, Product details, Cart pages',
+                            'Implemented Slider and Zoom in Product details page',
+                            'Added Pull Request Template',
                         ]}
                         github="sergiozeppo"
                     />
                     <DevCard
                         imgURL="../../images/dev-2.jpg"
-                        name="Sergey Tsepodoy"
-                        title="Team Lead, Frontend Developer"
+                        name="Vitaly Kim"
+                        title="Frontend Developer"
                         bio="I graduated with a degree in Computer Science. I have been studying frontend development
-                            since November 2023"
+                            since May 2023"
                         contributions={[
                             'Added development scripts and set up repository',
-                            'Set up Jest and perform test suites for entire project',
-                            'Developed Main, About Us, Product details pages',
+                            'Translated Commerce Tools Docs for normal people',
+                            'Added comphensive README to the project',
+                            'Developed Catalog, Login, Cart pages',
+                            'Set up products filtering, sorting and searching',
                         ]}
-                        github="sergiozeppo"
+                        github="ki8vi"
                     />
                     <DevCard
                         imgURL="../../images/dev-3.jpg"
-                        name="Sergey Tsepodoy"
-                        title="Team Lead, Frontend Developer"
-                        bio="I graduated with a degree in Computer Science. I have been studying frontend development
+                        name="Kirill Parfenov"
+                        title="Frontend Developer"
+                        bio="I currently studying Computer Science. I have been studying frontend development
                             since November 2023"
                         contributions={[
                             'Added development scripts and set up repository',
-                            'Set up Jest and perform test suites for entire project',
-                            'Developed Main, About Us, Product details pages',
+                            'Set up Commerce Tools Profile for project',
+                            'Developed Registration, Profile, Password change and Cart pages',
+                            'Set up inputs validation',
+                            'Implemented Infinite Scroll on the Catalog page',
+                            'Set up Routing',
                         ]}
-                        github="sergiozeppo"
+                        github="izeevens"
                     />
                 </div>
             </div>
