8831af52f2587de60bf4648647990df6a033fbfb
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-06-17 00:13:00 +0500

    fix: delete huge commented code

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -10,52 +10,6 @@ export default function Cart() {
         setCurrentCartProd(cart?.lineItems);
     }, [cart?.lineItems, cart]);
     return (
-        // <>
-        //     <table className="container-cart">
-        //         {/* <thead>
-        //             <tr className="cart-thead">
-        //                 <th className="cart-thead-item">Product</th>
-        //                 <th className="cart-thead-item">Price</th>
-        //                 <th className="cart-thead-item">Quantity</th>
-        //                 <th className="cart-thead-item">Total Price</th>
-        //             </tr>
-        //         </thead> */}
-        //         <tbody className="cart-wrapper">
-        //             {currentCartProd && currentCartProd.length > 0 ? (
-        //                 currentCartProd.map((item) => {
-        //                     const imageUrl = item.variant?.images?.[0]?.url || '';
-        //                     const price = item.variant?.prices?.[0]?.value?.centAmount ?? 0;
-        //                     const totalPrice = item.totalPrice.centAmount;
-        //                     const fixedPrice = price / 100;
-        //                     const fixedTotalPrice = totalPrice / 100;
-        //                     return (
-        //                         <CartCard
-        //                             key={item.id}
-        //                             images={imageUrl}
-        //                             name={item.name['en-US']}
-        //                             price={fixedPrice}
-        //                             quantity={item.quantity}
-        //                             totalPrice={fixedTotalPrice}
-        //                             id={item.id}
-        //                             version={cart.version}
-        //                             cartId={cart.id}
-        //                         />
-        //                     );
-        //                 })
-        //             ) : (
-        //                 <tr>
-        //                     <td className="nothing-title">Nothing Found!</td>
-        //                 </tr>
-        //             )}
-        //         </tbody>
-        //     </table>
-        //     <div className="container-cart-bottom">
-        //         <button className="cart-clear-btn button">Clear Cart</button>
-        //         <span className="cart-total">
-        //             Total Price: $ {cart?.totalPrice.centAmount && (cart?.totalPrice.centAmount / 100).toFixed(2)}
-        //         </span>
-        //     </div>
-        // </>
         <>
             {cart && (
                 <>
